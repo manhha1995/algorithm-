31fd13e501562e3c21a75025920c69edc309356b
Nguyen Manh Ha <manhhanguyen95@hotmail.com>
2024-11-20 14:58:28 +0700

    subarray leetcode

--- src/SlideWindow.java
+++ src/SlideWindow.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class SlideWindow {
     public int minimumSubArray(int[] arr, int n, int k) {
         if (n <= k) {
@@ -17,5 +19,63 @@ public class SlideWindow {
         }
 
         return minSum;
+    }
+
+    public static int maxSubArray(int[] arr, int k) {
+        int n = arr.length;
+        if (n <= k ) {
+            return 0;
+        }
+
+        int maxSum = 0;
+        HashSet<Integer> set = new HashSet<>();
+        int window_sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            window_sum += arr[i];
+            set.add(arr[i]);
+        }
+        if (set.size() == k) {
+            maxSum = window_sum;
+        }
+
+
+        for (int i = k; i < arr.length; i++) {
+            set.remove(arr[i-k]);
+            window_sum -= arr[i-k] ;
+
+            window_sum += arr[i];
+            set.add(arr[i]);
+
+            if (set.size() == k) {
+                maxSum = Math.max(maxSum, window_sum);
+            }
+
+            set.add(arr[i -k + 1]);
+        }
+
+        return maxSum; 
+    }
+
+    public static int minimizeArrayValue(int[] nums) {
+        int result = nums[0];
+        int prev = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int cur = nums[i];
+            if (cur >= prev) {
+                prev = cur;
+            } else {
+                int d = prev - cur;
+                int k = (d + i - 1) / i;
+                prev = cur + k;
+            }
+            result = Math.max(result, prev);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3,7,1,6};
+        System.out.println(minimizeArrayValue(a));
     }
 }
